b7b3094538404f954378c4712455e57decbcd688
Urtyom-Alyanov <mc2017artemklochkcov435@yandex.ru>
2025-11-20 19:31:08 +0300

    Add license

--- configuration/overlays.nix
+++ configuration/overlays.nix
@@ -1,11 +1,44 @@
-{ pkgs, inputs }: {
-  overlays = [
+{ inputs }:
+{
+  nixpkgs.overlays = [
     (final: prev: {
-      loginom-community = inputs.loginom.packages.${final.system}.loginom-community;
-      hyprland = inputs.hyprland.packages.${final.system}.hyprland;
-      xdg-desktop-portal-hyprland = inputs.hyprland.packages.${final.system}.xdg-desktop-portal-hyprland;
-      hyprlandPlugins = inputs.hyprland-plugins.packages.${final.system} // {
-        hypr-dynamic-cursors = inputs.hypr-dynamic-cursors.packages.${final.system}.hypr-dynamic-cursors;
+      flakes = {
+        loginom-community = inputs.loginom.packages.${final.system}.loginom-community;
+        hyprland = inputs.hyprland.packages.${final.system}.hyprland;
+        xdg-desktop-portal-hyprland = inputs.hyprland.packages.${final.system}.xdg-desktop-portal-hyprland;
+        hyprlandPlugins = inputs.hyprland-plugins.packages.${final.system} // {
+          hypr-dynamic-cursors = inputs.hypr-dynamic-cursors.packages.${final.system}.hypr-dynamic-cursors;
+        };
+      };
+
+      # Use standard NixOS repos
+      stable = import inputs.nixpkgs-stable {
+        inherit (final) system;
+        config = {
+          allowBroken = true;
+        };
+      };
+      unstable = import inputs.nixpkgs-unstable {
+        inherit (final) system;
+        config = {
+          allowBroken = true;
+        };
+      };
+
+      # Use NixOS repos with pr*prietary software
+      stable-proprietary = import inputs.nixpkgs-stable {
+        inherit (final) system;
+        config = {
+          allowBroken = true;
+          allowUnfree = true;
+        };
+      };
+      unstable-proprietary = import inputs.nixpkgs-unstable {
+        inherit (final) system;
+        config = {
+          allowBroken = true;
+          allowUnfree = true;
+        };
       };
     })
   ];
--- flake.nix
+++ flake.nix
@@ -2,8 +2,39 @@
   description = "Мой фембой сетап нв NixOS nya~~";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    # Nix pkgs
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.05";
+
+    nixpkgs.follows = "nixpkgs-unstable";
+
+    # Flake pkgs
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    hyprland = {
+      url = "github:hyprwm/hyprland";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    hyprland-plugins = {
+      url = "github:hyprwm/hyprland-plugins";
+      inputs.hyprland.follows = "hyprland";
+    };
+    hypr-dinamic-cursors = {
+      url = "github:VirtCode/hypr-dynamic-cursors";
+      inputs.hyprland.follows = "hyprland";
+    };
+    loginom = {
+      url = "github:Urtyom-Alyanov/loginom-community-package";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    # plasma-manager = {
+    #   url = "github:nix-community/plasma-manager";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    #   inputs.home-manager.follows = "home-manager";
+    # };
   };
   outputs = { nixpkgs, ... }@inputs: let
 
